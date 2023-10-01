a8fd244874c80f5622d20c82878544d8945c708e
irfanalvi67 <darkshadow0067@gmail.com>
2023-10-01 08:17:45 -0700

    Added eslint rules (auto end of line & disabled prop type declartion)

---
+++ src/layout/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = ({ children }) => {
+  return (
+    <div>
+      <h2>In Layout</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/pages/Index.js
@@ -0,0 +1,2 @@
+export { default as Dashboard } from "./dashboard/Index";
+export { default as Settings } from "./settings/Index";
